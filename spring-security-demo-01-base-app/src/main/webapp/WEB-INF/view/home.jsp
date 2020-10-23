ff3aedec0746dc2ff9cfe3a0e1e2ed43e8d5f460
Maycon Ribeiro <maycon_ribeiro@hotmail.com>
2020-10-23 19:48:25 -0300

    Spring Security start

---
+++ spring-security-demo-01-base-app/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Hello Spring Securty</title>
+</head>
+<body>
+	<h2>Hello World Company</h2>
+	
+	<hr>
+	
+	Welcome Spring Security module 
+	
+</body>
+</html>
---
+++ spring-security-demo-02-basic-security/src/main/java/br/com/springsec/demo/config/MySpringDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package br.com.springsec.demo.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { DemoAppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
