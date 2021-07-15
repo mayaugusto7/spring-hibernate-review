23a13596d050b8890fdb179345f5a3d41f96a37d
Maycon Augusto <mayaugusto777@gmail.com>
2021-07-15 11:24:55 -0300

    Spring Security projects

---
+++ spring-security-demo-06-csrf/src/main/java/br/com/springsec/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package br.com.springsec.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
