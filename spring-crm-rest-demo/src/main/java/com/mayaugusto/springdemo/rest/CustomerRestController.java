c91216f796e0e4be5dfc1e34bf0cebcc725c8894
Maycon Augusto <4804325+mayaugusto7@users.noreply.github.com>
2021-10-26 21:16:32 -0300

    Finish Spring Rest

--- spring-crm-rest-demo/src/main/java/com/mayaugusto/springdemo/rest/CustomerRestController.java
+++ spring-crm-rest-demo/src/main/java/com/mayaugusto/springdemo/rest/CustomerRestController.java
@@ -3,8 +3,12 @@ package com.mayaugusto.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,4 +43,37 @@ public class CustomerRestController {
 		return theCustomer;
 	}
 	
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {
+		
+		// Also just in cadse the pass id in JSON ... set id to 0
+		// This is force a save of new item ... instead of update
+		theCustomer.setId(0);
+		
+		this.customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+		
+		this.customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}
+	
+	@DeleteMapping("/customers/{customerId}")
+	public String deleteCustomer(@PathVariable int customerId) {
+		
+		Customer customer = this.customerService.getCustomer(customerId);
+		
+		if (customer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		this.customerService.deleteCustomer(customerId);
+		
+		return "Deleted customer id - " + customerId;
+	}
 }
